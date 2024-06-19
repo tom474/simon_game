318ea91f203f19a7828a5c0f5e44277d95562228
tom474 <emsignouc0407@gmail.com>
2024-06-19 22:50:54 +0700

    Adjust the animation

--- game.js
+++ game.js
@@ -6,19 +6,7 @@ function nextSequence() {
 	var randomChosenColor = buttonColors[randomNumber];
 	gamePattern.push(randomChosenColor);
 
-	$("#" + randomChosenColor)
-		.fadeOut(100)
-		.fadeIn(100);
+	$("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
 	var audio = new Audio("./sound/" + randomChosenColor + ".mp3");
 	audio.play();
 }
-
-var randomNumber = Math.floor(Math.random() * 4);
-var randomChosenColor = buttonColors[randomNumber];
-$("#" + randomChosenColor).on("click", function () {
-	$("#" + randomChosenColor)
-		.fadeOut(100)
-		.fadeIn(100);
-	var audio = new Audio("./sounds/" + randomChosenColor + ".mp3");
-	audio.play();
-});
