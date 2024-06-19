3dee8e98fdc6b418189b291a01ef530d7c1eb970
tom474 <emsignouc0407@gmail.com>
2024-06-19 22:49:15 +0700

    Add animation and sounds

--- game.js
+++ game.js
@@ -2,7 +2,23 @@ var buttonColors = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 
 function nextSequence() {
-    var randomNumber = Math.floor(Math.random() * 4);
-    var randomChosenColor = buttonColors[randomNumber];
-    gamePattern.push(randomChosenColor);
+	var randomNumber = Math.floor(Math.random() * 4);
+	var randomChosenColor = buttonColors[randomNumber];
+	gamePattern.push(randomChosenColor);
+
+	$("#" + randomChosenColor)
+		.fadeOut(100)
+		.fadeIn(100);
+	var audio = new Audio("./sound/" + randomChosenColor + ".mp3");
+	audio.play();
 }
+
+var randomNumber = Math.floor(Math.random() * 4);
+var randomChosenColor = buttonColors[randomNumber];
+$("#" + randomChosenColor).on("click", function () {
+	$("#" + randomChosenColor)
+		.fadeOut(100)
+		.fadeIn(100);
+	var audio = new Audio("./sounds/" + randomChosenColor + ".mp3");
+	audio.play();
+});
