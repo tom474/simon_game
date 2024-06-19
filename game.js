22725b68b811b22c6baa996e431703fb25e1122a
tom474 <emsignouc0407@gmail.com>
2024-06-19 19:34:17 +0700

    Create a new pattern

---
+++ game.js
@@ -0,0 +1,8 @@
+var buttonColors = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+
+function nextSequence() {
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColor = buttonColors[randomNumber];
+    gamePattern.push(randomChosenColor);
+}
