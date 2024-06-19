6670a6c9054fda36f87576da86f674d8c662d291
tom474 <emsignouc0407@gmail.com>
2024-06-19 23:01:53 +0700

    Check which button is pressed

--- game.js
+++ game.js
@@ -1,5 +1,12 @@
 var buttonColors = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
+var userClickedPattern = [];
+
+$(".btn").on("click", function () {
+    var userChosenColor = $(this).attr("id");
+    userClickedPattern.push(userChosenColor);
+    // console.log(userClickedPattern);
+});
 
 function nextSequence() {
 	var randomNumber = Math.floor(Math.random() * 4);
@@ -7,6 +14,7 @@ function nextSequence() {
 	gamePattern.push(randomChosenColor);
 
 	$("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+
 	var audio = new Audio("./sound/" + randomChosenColor + ".mp3");
 	audio.play();
 }
