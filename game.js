0f4baba7e346a7e060843efc15324c986d3b4bba
tom474 <emsignouc0407@gmail.com>
2024-06-20 22:55:54 +0700

    Check the user's answer against the game sequence

--- game.js
+++ game.js
@@ -18,9 +18,11 @@ $(".btn").on("click", function () {
 
 	playSound(userChosenColor);
 	animatePress(userChosenColor);
+	checkAnswer(userClickedPattern.length - 1);
 });
 
 function nextSequence() {
+	userClickedPattern = [];
 	level++;
 	$("#level-title").text("Level " + level);
 
@@ -45,4 +47,17 @@ function animatePress(currentColor) {
 	setTimeout(function () {
 		$("#" + currentColor).removeClass("pressed");
 	}, 100);
+}
+
+function checkAnswer(currentLevel) {
+	if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+		console.log("success");
+		if (userClickedPattern.length === gamePattern.length) {
+			setTimeout(function () {
+				nextSequence();
+			}, 1000);
+		}
+	} else {
+		console.log("wrong");
+	}
 }
